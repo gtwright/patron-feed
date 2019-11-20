cf9cc8521039acebeab28dc10ba768c7205407b4
Graham <gtwright@gmail.com>
2019-11-20 13:23:04 -0500

    add othercal feed

--- handler.js
+++ handler.js
@@ -1,38 +1,58 @@
 const getFeed = require("./lib/getPatronFeed");
 const makeiCal = require("./lib/iCalByInstance");
+const makeOther = require("./lib/otherCalByInstance");
 
 module.exports.ical = (event, context, callback) => {
-	const url = `${process.env.feedDomain}/ticket/PatronTicket__PublicApiEventList`;
-	getFeed(url)
-		.then(async feed => {
-			const message = await makeiCal(feed.data);
-			const response = {
-				statusCode: 200,
-				body: message
-			};
-			callback(null, response);
-		})
-		.catch(error => {
-			callback(error, {
-				success: false
-			});
-		});
+  const url = `${process.env.feedDomain}/ticket/PatronTicket__PublicApiEventList`;
+  getFeed(url)
+    .then(async feed => {
+      const message = await makeiCal(feed.data);
+      const response = {
+        statusCode: 200,
+        body: message
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      callback(error, {
+        success: false
+      });
+    });
+};
+
+module.exports.otherCal = (event, context, callback) => {
+  const url = `${process.env.feedDomain}/ticket/PatronTicket__PublicApiEventList`;
+  getFeed(url)
+    .then(async feed => {
+      const message = await makeOther(feed.data);
+      //   const message = "{ test: 'message' }";
+      const response = {
+        statusCode: 200,
+        body: message
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      callback(error, {
+        success: false
+      });
+    });
 };
 
 module.exports.redirect = async event => {
-	const response = {
-		statusCode: 301,
-		headers: {
-			Location: "https://services.opusaffair.com"
-		}
-	};
+  const response = {
+    statusCode: 301,
+    headers: {
+      Location: "https://services.opusaffair.com"
+    }
+  };
 
-	return response;
+  return response;
 };
 
 module.exports.hello = async event => {
-	return {
-		statusCode: 200,
-		body: JSON.stringify({ message: "Connected" })
-	};
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Connected" })
+  };
 };
