4370212cae042148836f8b7aa685497721af650b
Graham <gtwright@gmail.com>
2019-12-06 20:14:12 -0500

    othercal update to rest reult

--- handler.js
+++ handler.js
@@ -27,11 +27,11 @@ module.exports.otherCal = (event, context, callback) => {
       const message = await makeOther(feed.data);
       //   const message = "{ test: 'message' }";
       const response = {
-        headers: {
-          "Content-Type": "text/html"
-        },
+        // headers: {
+        //   "Content-Type": "text/html"
+        // },
         statusCode: 200,
-        body: message
+        body: JSON.stringify(message)
       };
       callback(null, response);
     })
